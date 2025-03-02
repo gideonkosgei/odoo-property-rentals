c064e34a108136ac95f1b666c6fe220bb0d3ff58
gideonkosgei <mr.gkosgei@gmail.com>
2025-03-02 17:56:32 +0300

    remove modifier when toggle visibility of objects

--- static/src/js/hr_employee_kanban.js
+++
@@ -1,8 +0,0 @@
-/** @odoo-module **/
-import { registry } from "@web/core/registry";
-import { KanbanView } from "@web/views/kanban/kanban_view";
-
-export class HrEmployeeKanban extends KanbanView {}
-
-registry.category("views").add("hr_employee_kanban", HrEmployeeKanban);
-
