7daee2b5b7dccb69588934197641e8ef0ac56cf0
gideonkosgei <mr.gkosgei@gmail.com>
2025-03-12 09:34:29 +0300

    implemented unit specs

--- models/property_unit.py
+++ models/property_unit.py
@@ -9,6 +9,65 @@ class PropertyUnit(models.Model):
     name = fields.Char(string="Unit Name", required=True, help="The name or number of the unit (e.g., A101, Office 3B)")
     property_id = fields.Many2one("property.property", string="Property", required=True,
                                   help="The property this unit belongs to")
+
+    image_ids = fields.One2many(
+        "property.unit.image", "unit_id", string="Unit Images"
+    )
+
+    utility_meter_ids = fields.One2many("property.unit.utility.meter", "unit_id", string="People")
+
+    wall_material = fields.Many2one(
+        "property.list.value",
+        string="Wall Material",
+        domain="[('list_name_id', '=', 54), ('active', '=', True)]",
+        tracking=True
+    )
+    flooring_type = fields.Many2one(
+        "property.list.value",
+        string="Flooring Type",
+        domain="[('list_name_id', '=', 55), ('active', '=', True)]",
+        tracking=True
+    )
+    kitchen_type = fields.Many2one(
+        "property.list.value",
+        string="Kitchen Type",
+        domain="[('list_name_id', '=', 56), ('active', '=', True)]",
+        tracking=True
+    )
+    kitchen_appliances = fields.Many2one(
+        "property.list.value",
+        string="Kitchen Appliances",
+        domain="[('list_name_id', '=', 57), ('active', '=', True)]",
+        tracking=True
+    )
+    cabinets_material = fields.Many2one(
+        "property.list.value",
+        string="cabinet Material",
+        domain="[('list_name_id', '=', 58), ('active', '=', True)]",
+        tracking=True
+    )
+    lighting_type = fields.Many2one(
+        "property.list.value",
+        string="Lighting Type",
+        domain="[('list_name_id', '=', 59), ('active', '=', True)]",
+        tracking=True
+    )
+    air_conditioning = fields.Many2one(
+        "property.list.value",
+        string="Air Conditioning",
+        domain="[('list_name_id', '=', 60), ('active', '=', True)]",
+        tracking=True
+    )
+    heating_system = fields.Many2one(
+        "property.list.value",
+        string="Heating System",
+        domain="[('list_name_id', '=', 61), ('active', '=', True)]",
+        tracking=True
+    )
+    fireplace = fields.Boolean(string="Fireplace", default=False)
+
+
+
     # unit_type_category_id = fields.Many2one("property.list.name", string="Unit Category",
     #                                         required=True, domain="[('list_type_id', '=', 2), ('active', '=', True)]")
     # unit_type_id = fields.Many2one("property.list.value", string="Unit Type", required=True,
@@ -33,11 +92,7 @@ class PropertyUnit(models.Model):
         default="available",
     )
 
-    image_ids = fields.One2many(
-        "property.unit.image", "unit_id", string="Unit Images"
-    )
 
-    utility_meter_ids = fields.One2many("property.unit.utility.meter", "unit_id", string="People")
 
     # Parking
     # parking_space_id = fields.Many2one("property.parking", string="Assigned Parking Space",
